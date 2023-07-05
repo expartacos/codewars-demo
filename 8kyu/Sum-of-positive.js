fb7f653cb26d2e0b05093fafa2e825fbdd35fa74
Rafael Pereira <rafaelpersan@gmail.com>
2023-07-05 11:01:55 -0300

    added new codewars challenge

---
+++ 8kyu/Sum-of-positive.js
@@ -0,0 +1,20 @@
+/*DESCRIPTION:
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.*/
+
+//My solution
+
+function positiveSum(arr) {
+    let sum = 0;
+    for ( let i = 0; i < arr.length; i++){
+      if(arr[i] < 0){
+        arr.splice([i], 1, 0);
+      }else{
+        sum += arr[i];
+      }   
+    }
+    return sum;
+  }
