3b6c3ab9b14f35bdc9823a0913a5c233ec5b6ae1
Rafael Pereira <rafaelpersan@gmail.com>
2023-07-05 10:42:32 -0300

    added new codewars challenge

---
+++ 8kyu/Opposite-number.js
@@ -0,0 +1,12 @@
+/*DESCRIPTION:
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34*/
+
+//Minha solução 
+
+let opposite = (number)=> {return number * -1;}
