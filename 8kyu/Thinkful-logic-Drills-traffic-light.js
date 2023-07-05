95c0e1225827fcf8a6432eb16c8ff5466ab2298e
Rafael Pereira <rafaelpersan@gmail.com>
2023-07-05 11:31:10 -0300

    added new codewars challenge

---
+++ 8kyu/Thinkful-logic-Drills-traffic-light.js
@@ -0,0 +1,23 @@
+/*DESCRIPTION:
+You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, when the input is green, output should be yellow.*/
+
+
+//my solution 
+
+function updateLight(current) {
+  
+    //your code here!
+    switch (current){
+        case 'green':
+          return 'yellow';
+        case 'yellow': 
+          return 'red';
+        case 'red': 
+          return 'green';
+    }
+  
+  }
