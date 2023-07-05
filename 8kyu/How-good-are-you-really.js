2299ed7ff132b0ec59976e74897961d2224c926c
Rafael Pereira <rafaelpersan@gmail.com>
2023-07-05 11:27:17 -0300

    added a new codewars challenge

---
+++ 8kyu/How-good-are-you-really.js
@@ -0,0 +1,35 @@
+/*DESCRIPTION:
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!*/
+
+//My solution 
+
+
+function betterThanAverage(classPoints, yourPoints) {
+    let contador = classPoints.length;
+    let total = 0;
+    let media = 0;
+    for (let i in classPoints){
+      total += classPoints[i];    
+    }
+    media = total/contador;
+    if (media < yourPoints){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+
+  //second solution 
+
+  function betterThanAverage(classPoints, yourPoints) {
+    return yourPoints > classPoints.reduce((a, b) => a+b, 0)/classPoints.length;
+  }
