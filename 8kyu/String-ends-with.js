39af040bcad465d32c79fb69c188ab248ed1ef9c
Rafael Pereira <rafaelpersan@gmail.com>
2023-07-10 07:55:48 -0300

    added new codewars challenge

--- 8kyu/String-ends-with.js
+++ 8kyu/String-ends-with.js
@@ -1,5 +1,14 @@
+/*DESCRIPTION:
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false*/
+
+
 function solution(str, ending){
-  if(str.substring(str.length -2) === ending){
+  if(str.substring(str.length -ending.length) === ending){
     return true;
   }else{
     return false;
