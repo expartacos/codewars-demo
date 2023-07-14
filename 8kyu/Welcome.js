ba6fd42f04edf4d8f9189cee2756c8b1c7a37648
Rafael Pereira <rafaelpersan@gmail.com>
2023-07-13 22:46:04 -0300

    added new codewars challenge

--- 8kyu/Welcome.js
+++ 8kyu/Welcome.js
@@ -1,23 +1,26 @@
 function greet(language) {
-	let saudacao = {
-english: 'Welcome',
-czech: 'Vitejte',
-danish: 'Velkomst',
-dutch: 'Welkom',
-estonian: 'Tere tulemast',
-finnish: 'Tervetuloa',
-flemish: 'Welgekomen',
-french: 'Bienvenue',
-german: 'Willkommen',
-irish: 'Failte',
-italian: 'Benvenuto',
-latvian: 'Gaidits',
-lithuanian: 'Laukiamas',
-polish: 'Witamy',
-spanish: 'Bienvenido',
-swedish: 'Valkommen',
-welsh: 'Croeso'
-  };
-  
-  return saudacao.language;
+	var GreetingsDB = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }, 
+  defaultLanguage = 'english';
+console.log( GreetingsDB[language]) || GreetingsDB[defaultLanguage];
 }
+
+
+  greet('italian');
