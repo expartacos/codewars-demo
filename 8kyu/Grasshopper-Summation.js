fdcfbb8bf616b69e153c3976ea9af98ec9635f33
Rafael Pereira <rafaelpersan@gmail.com>
2023-07-14 17:09:05 -0300

    added a new codewars challenge

---
+++ 8kyu/Grasshopper-Summation.js
@@ -0,0 +1,15 @@
+var summation = function (num) {
+    let a = 0;
+    let soma = 0;
+    for (let i = 0; i <= num; i++){
+
+        a++;
+
+
+    }
+    soma = num*(a/2)
+    return soma;
+}
+
+
+  summation(8);
