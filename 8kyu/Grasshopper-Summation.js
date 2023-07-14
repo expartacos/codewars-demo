d990faad39f8a2ec7c6d2ffa9cf698a14acdeacf
Rafael Pereira <rafaelpersan@gmail.com>
2023-07-14 17:11:12 -0300

    added a new codewars challenge

--- 8kyu/Grasshopper-Summation.js
+++ 8kyu/Grasshopper-Summation.js
@@ -1,3 +1,12 @@
+/*Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+For example (Input -> Output):
+
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)*/
+
+
+
 var summation = function (num) {
     let a = 0;
     let soma = 0;
