3aab9e0686501ac0a238771b99816b160c119e98
Rafael Pereira <rafaelpersan@gmail.com>
2023-07-28 16:45:36 -0300

    added new codewars challenge

--- 8kyu/Switch/Case.js
+++ 8kyu/Switch/Case.js
@@ -1,3 +1,6 @@
+/*Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?*/
+
+
 function evalObject(value){
     var result = 0;
     switch(value.operation){
