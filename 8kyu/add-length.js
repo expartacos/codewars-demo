9848751ead41ed96bf5008586787b430627987ef
Rafael Pereira <rafaelpersan@gmail.com>
2023-08-01 13:51:13 -0300

    added new codewars challenge

--- 8kyu/add-length.js
+++ 8kyu/add-length.js
@@ -1,6 +1,13 @@
 function addLength(str){
     let a = str.split(' ');
     console.log(a);
+    let b = [];
+    for(let i in a){
+        let c = a[i].toString();
+        console.log(c.length)
+        b.push( a[i] + ' ' + c.length );
+    }
+    console.log(b);
 }
 
 addLength("rafa garcia rafa");
