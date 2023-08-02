86660140640abd98ad76e499b49c00ec9455a980
Rafael Pereira <rafaelpersan@gmail.com>
2023-08-02 15:30:02 -0300

    added a new codewars challenge

---
+++ 8kyu/Hello-Happy-Codevarrior!.js
@@ -0,0 +1,12 @@
+function Warrior(n){
+    var name = n;  
+    this.name = function(n){
+      if( n ) name=n;
+      return name;
+    }
+  }
+    
+  Warrior.prototype.toString = function(){
+      return "Hi! my name's "+this.name();
+  }
+  
