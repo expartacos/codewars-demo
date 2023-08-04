09ac6af8c937c24d750dbfa0b4ba1bd3a267c049
Rafael Pereira <rafaelpersan@gmail.com>
2023-08-04 15:46:16 -0300

    added new codewars challenge

--- 8kyu/Is this my tail.js
+++ 8kyu/Is this my tail.js
@@ -2,7 +2,7 @@ function correctTail(body, tail){
   
     let sub = body.substr(body.length-(tail.length));
     
-    if (sub = tail){
+    if (sub == tail){
       return true
     }else 
       return false
