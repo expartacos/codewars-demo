bbec281a3dcfe277eb7a47b9f3df478c865e29e7
Rafael Pereira <rafaelpersan@gmail.com>
2023-08-09 09:34:39 -0300

    added new codewars challenge

--- 8kyu/multiply the number.js
+++ 8kyu/multiply the number.js
@@ -1,12 +1,12 @@
-const multiply = (number)=> a = number * (5**number.toString().length);
+const multiply = (number)=> number >= 0? a = number * (5**number.toString().length): a = number * (5**(number.toString().length - 1));
 
 
 
-console.log(multiply(10));
+console.log(multiply(-2));
 
 function mult(num){
     let a = num.toString();
     let b = num * (5**a.length);
     console.log(b);
 }
-mult(10);
+mult(-10);
