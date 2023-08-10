d37f15e0cb1c3f1f6ce4cfba9ab371e8b3a5fbf8
Rafael Pereira <rafaelpersan@gmail.com>
2023-08-10 16:53:39 -0300

    added new codewars challenge

---
+++ 8kyu/Get Planet Name By ID.js
@@ -0,0 +1,13 @@
+function getPlanetName(id){
+    switch(id){
+      case 1: return 'Mercury';
+      case 2: return 'Venus';
+      case 3: return'Earth';
+      case 4: return'Mars';
+      case 5: return'Jupiter';
+      case 6: return'Saturn';
+      case 7: return'Uranus';
+      case 8: return'Neptune';
+    }
+  
+  }
