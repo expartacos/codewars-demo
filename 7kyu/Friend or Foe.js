06855326be9ab48435ba0ac031f57df9cca54dfd
Rafael Pereira <rafaelpersan@gmail.com>
2023-08-15 12:43:44 -0300

    added a new codewars challenge

--- 7kyu/Friend or Foe.js
+++ 7kyu/Friend or Foe.js
@@ -1,2 +1,12 @@
-const friend = (friends) => friends.length === 4 ? friends: null;
 
+let a = ["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"];
+let nomes = a.map(amigo);
+
+function amigo(elemento){
+    let b = []
+    if(elemento.length === 4 && (typeof elemento) === "string"){
+        return b.push(elemento);
+    }
+}
+
+console.log (nomes);
