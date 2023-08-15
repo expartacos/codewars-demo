2e452991387fc238ffe9c015282371519083d84d
Rafael Pereira <rafaelpersan@gmail.com>
2023-08-15 16:30:46 -0300

    added a new codewars challenge

--- 7kyu/Get Full Name.js
+++ 7kyu/Get Full Name.js
@@ -1,3 +1,17 @@
+/*The code provided is supposed return a person's Full Name given their first and last names.
+
+But it's not working properly.
+
+Notes
+The first and/or last names are never null, but may be empty.
+
+Task
+Fix the bug so we can all go home early.*/
+
+
+
+
+
 class Dinglemouse{
 
     constructor( firstName, lastName ){
